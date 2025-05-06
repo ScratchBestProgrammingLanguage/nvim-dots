86319d41d26296cd35a3f736a040ade377dc0b8f
ScratchBestProgrammingLanguage <claclagamer.pratesi@gmail.com>
2025-05-06 19:39:07 +0200

    Removed slint support

--- lua/configs/autocmd.lua
+++ lua/configs/autocmd.lua
@@ -4,14 +4,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         vim.lsp.buf.format()
     end
 })
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*.slint",
-    callback = function()
-        local buf = vim.api.nvim_get_current_buf()
-        vim.api.nvim_set_option_value('filetype', 'slint', { buf = buf })
-    end
-})
-
--- Slint support
--- autocmd BufEnter *.slint :setlocal filetype=slint
